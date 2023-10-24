a92c9d3d8ed3bd3b8ca3fc4056a75396c6b4d16c
SzymonW66 <szymekwajs@gmail.com>
2023-10-24 12:12:18 +0200

    Initial Commit

---
+++ src/test/java/com/example/springlearning2/SpringLearning2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlearning2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearning2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
