354f5268bf5aaf88ff0ea7774c907ac2ce07da7d
SzymonW66 <szymekwajs@gmail.com>
2023-10-24 13:55:21 +0200

    Moduł: Kontener Wstrzykiwania zależności Lekcja 5: Własności i pliki propeties

---
+++ src/main/java/com/example/springlearning2/InvalidDecoratorException.java
@@ -0,0 +1,7 @@
+package com.example.springlearning2;
+
+public class InvalidDecoratorException extends RuntimeException {
+    public InvalidDecoratorException(String message ) {
+            super(message);
+    }
+}
