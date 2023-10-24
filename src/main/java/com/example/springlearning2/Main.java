5e1bd7a8240690236dad104b05e8e3c35869bc52
SzymonW66 <szymekwajs@gmail.com>
2023-10-24 13:47:06 +0200

    Moduł: Kontener Wstrzykiwania zależności Lekcja 5: Klasy konfiguracji

--- src/main/java/com/example/springlearning2/Main.java
+++ src/main/java/com/example/springlearning2/Main.java
@@ -3,6 +3,9 @@ package com.example.springlearning2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class Main {
@@ -10,5 +13,10 @@ public class Main {
         ConfigurableApplicationContext context = SpringApplication.run(Main.class);
         MessagePrinter messagePrinter = context.getBean(MessagePrinter.class);
         messagePrinter.printMessage();
+    }
+
+    @Bean
+    Scanner scanner(){
+       return new Scanner(System.in);
     }
 }
