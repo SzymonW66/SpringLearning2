a1b524bef9fa19c511d8d28647ef8cc803b18d38
SzymonW66 <szymekwajs@gmail.com>
2023-10-24 12:28:38 +0200

    Moduł: Kontener Wstrzykiwania zależności Lekcja 1: Wstrzykiwanie zależności

--- src/main/java/com/example/springlearning2/Main.java
+++ src/main/java/com/example/springlearning2/Main.java
@@ -1,4 +1,9 @@
 package com.example.springlearning2;
 
 public class Main {
+    SimpleMessageProducer messageProducer = new SimpleMessageProducer();
+    MessagePrinter firstMessagePrinter = new MessagePrinter(messageProducer);
+    MessagePrinter secondMessagePrinter = new MessagePrinter(messageProducer);
+        firstMessagePrinter.printMessage();
+        secondMessagePrinter.printMessage();
 }
